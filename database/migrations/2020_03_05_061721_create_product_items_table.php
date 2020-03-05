83daa748aca89149b10e54403086263c750b631d
Anton <antopyvo87@gmail.com>
2020-03-05 08:52:26 +0200

    create model and migration

---
+++ database/migrations/2020_03_05_061721_create_product_items_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+
+            $table->string('slug')->unique()->nullable();
+            $table->string('title');
+            $table->string('price');
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw');
+            $table->string('title_image')->nullable();
+            $table->mediumText('image')->nullable();
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('product_categories');
+            $table->index('is_published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_items');
+    }
+}
